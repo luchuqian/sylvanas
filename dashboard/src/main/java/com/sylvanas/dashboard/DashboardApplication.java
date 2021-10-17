1e891f11da7a945f7c4b0fa570f5795b49d9f0b2
sylvanas <2226372177@qq.com>
2021-10-17 22:13:39 +0800

    清楚多余结构

--- dashboard/src/main/java/com/sylvanas/dashboard/DashboardApplication.java
+++ dashboard/src/main/java/com/sylvanas/dashboard/DashboardApplication.java
@@ -1,13 +1,13 @@
-package com.example.common;
+package com.sylvanas.dashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CommonApplication {
+public class DashboardApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CommonApplication.class, args);
+        SpringApplication.run(DashboardApplication.class, args);
     }
 
 }
