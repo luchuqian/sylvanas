5ee60db0e5de81d6ebac21dafa3d492e8804ba6b
sylvanas <2226372177@qq.com>
2022-01-09 21:46:10 +0800

    初始化docker

--- leetcode/src/main/java/com/sylvanas/leetcode/medium/TwoSum.java
+++ leetcode/src/main/java/com/sylvanas/leetcode/medium/TwoSum.java
@@ -83,6 +83,9 @@ public class TwoSum {
     }
 
     public static void main(String[] args) {
+        System.out.println(Long.MAX_VALUE);
+
+
         ListNode l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
         ListNode l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
         System.out.println(addTwoNumbers(l1, l2));
