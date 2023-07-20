3d90c1d32ab670e655e999a36c91e114196304e6
sylvanas <2226372177@qq.com>
2023-07-20 18:18:25 +0800

    n个一组反转链表

--- leetcode/src/main/java/com/sylvanas/link/algorithm/SwapPairsLinkNode.java
+++ leetcode/src/main/java/com/sylvanas/link/algorithm/SwapPairsLinkNode.java
@@ -14,19 +14,55 @@ public class SwapPairsLinkNode {
 
   }
 
+
+  public static SingleLinkListNode swapTwo(SingleLinkListNode head) {
+    if (head == null || head.next == null) {
+      return head;
+    }
+    SingleLinkListNode first = head;
+    SingleLinkListNode second = head.next;
+    first.next = swapTwo(second.next);
+    second.next = first;
+    return second;
+  }
+
+
+  /**
+   * n个节点一组反转链表
+   *
+   */
+  public static SingleLinkListNode swanN(SingleLinkListNode head, SingleLinkListNode preNode, int n) {
+    if (head == null || head.next == null) {
+      return head;
+    }
+    int num = n;
+
+    SingleLinkListNode first = head;
+    while (n > 0) {
+      SingleLinkListNode next = head.next;
+      head.next = preNode;
+      preNode = head;
+      head = next;
+      n--;
+    }
+    first.next = swanN(head, preNode, num);
+    return preNode;
+  }
+
   public static void main(String[] args) {
     SingleLinkListNode node = new SingleLinkListNode(
         new SingleLinkListNode(
             new SingleLinkListNode(
                 new SingleLinkListNode(
                     new SingleLinkListNode(
-                        new SingleLinkListNode(null, 5),
+                        new SingleLinkListNode(new SingleLinkListNode(null, 6),
+                            5),
                         4),
                     3),
                 2)
             , 1),
         0);
-    System.out.println(swapPairs(node));
+    System.out.println(swanN(node, null, 3));
   }
 
 }
