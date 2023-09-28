e7e2378b9c9182b2e031ca361173eb33fb8d0c96
sylvanas <2226372177@qq.com>
2023-09-28 12:20:13 +0800

    最短编辑距离

--- leetcode/src/main/java/com/sylvanas/leetcode/hard/MinDistance.java
+++ leetcode/src/main/java/com/sylvanas/leetcode/hard/MinDistance.java
@@ -1,8 +1,52 @@
 package com.sylvanas.leetcode.hard;
 
+import java.util.Arrays;
+
 public class MinDistance {
 
-  public int minDistance(String s1, String s2) {
+  private int[][] memo;
+
+  public int minDistanceUsingMemo(String s1, String s2) {
+    int firstLength = s1.length();
+    int secondLength = s2.length();
+    memo = new int[firstLength][secondLength];
+    Arrays.fill(memo, -1);
+    return dp(s1, firstLength - 1, s2, secondLength - 1);
+  }
+
+  /**
+   * 自顶向下 先列父问题代数解 递归到子问题解 从最优子结构求解
+   */
+  public int dp(String s1, int i, String s2, int j) {
+    // 两者都为-1时 返回值为0 不影响结果
+    if (i == -1) {
+      // 因为走完了s1 要让两者相等 就是删除或插入对应的剩余字符串长度 下标换算为操作数就是 数组下标+1(因为数组是从后向前遍历)
+      return j + 1;
+    }
+    if (j == -1) {
+      // 因为走完了s2 要让两者相等 就是删除或插入对应的剩余字符串长度 下标换算为操作数就是 数组下标+1(因为数组是从后向前遍历)
+      return i + 1;
+    }
+
+    if (memo[i][j] != -1) {
+      return memo[i][j];
+    }
+
+    if (s1.charAt(i) == s2.charAt(j)) {
+      memo[i][j] = dp(s1, i - 1, s2, j - 1);
+    } else {
+      memo[i][j] = min(dp(s1, i, s2, j - 1) + 1, // s1插入字符
+          dp(s1, i - 1, s2, j) + 1, // s1删除字符
+          dp(s1, i - 1, s2, j - 1) + 1 // s1替换字符
+      );
+    }
+    return memo[i][j];
+  }
+
+  /**
+   * 自底向上 先求子问题解 逐步累加到最后的父问题
+   */
+  public int minDistanceUsingDp(String s1, String s2) {
     int m = s1.length(), n = s2.length();
     int[][] dp = new int[m + 1][n + 1];
     // base case
